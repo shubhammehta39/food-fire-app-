e219d8701eb326d0418543277cb96ed261f18e2f
Shubham <shubhammehta2003@gmail.com>
2023-02-24 00:29:11 +0530

    secondcommit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 
-import './App.css';
+// import './App.css';
 
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>hello</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+// import ReactDOM from "react-dom/client";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+// import Footer from "../Components/Footer";
+
+/* My Food App structure will look like this, 
+            1) Header
+                - Logo
+                - Nav Items(right side)
+                - Cart
+            2) Body
+                - Search bar
+                - Restaurants List
+                    - Restaurant card
+                        - Image
+                        - Name
+                        - Rating
+            3) Footer
+                - Links
+                - Copyrights
+       
+*/
+
+// AppLayout component to render: Header, Body and Footer Component
 const App = () => {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Body />
+      {/* <Footer /> */}
+    </React.Fragment>
   );
-}
-
+};
 export default App;
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
---
+++ src/Components/Header.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import unnamed from "../images/unnamed.jpg";
+import "./style.css"
+
+// Title component for display logo
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      src={unnamed}
+      alt="Food Fire Logo"
+      title="Food Fire"
+    />
+  </a>
+);
+
+// Header component for header section: Logo, Nav Items
+const Header = () => {
+  // use useState for user logged in or logged out
+  const [isLoggedin, setIsLoggedin] = useState(true);
+
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
+          <li>
+            {/* use conditional rendering for login and logout */}
+            {isLoggedin ? (
+              <button
+                className="logout-btn"
+                onClick={() => setIsLoggedin(false)}
+              >
+                Logout
+              </button>
+            ) : (
+              <button className="login-btn" onClick={() => setIsLoggedin(true)}>
+                Login
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
