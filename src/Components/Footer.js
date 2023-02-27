f47dae0086cb037882c9fa005b19ccdfcc1cf58b
Shubham <shubhammehta2003@gmail.com>
2023-02-28 00:49:19 +0530

    changes

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+// Footer component for footer section
+const Footer = () => {
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a href="https://www.linkedin.com/in/shubhammehta/" target="_blank">
+        Shubham
+      </a>
+      <i className="fa-solid fa-copyright"></i>2023
+      <strong>
+        Food<span>Fire</span>
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
