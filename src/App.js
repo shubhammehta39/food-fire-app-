358a86f4e08db4bd63293b7411a65ce16805f280
Shubham <shubhammehta2003@gmail.com>
2023-03-16 23:29:39 +0530

    latest change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import './index.css';
 import React from 'react';
-// import ReactDOM  from 'react-dom/client';
-import Header from "./Components/Header"
-import Body from "./Components/Body"
-import Footer from './Components/Footer';
+// import ReactDOM from 'react-dom/client';
+
+// import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+// import About from './Components/About';
+// import Contact from './Components/Contact';
+// import RestaurantMenu from './Components/RestaurantMenu';
+// import Navbar from './Components/Navbar';
+// import Body from "./Components/Body";
+
+import Header from "./Components/Header";
+// import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
       {/* <h1>hi lets start</h1> */}
+      {/* <Navbar/> */}
       <Header />
-      <Body/>
-      <Footer />
+      {/* <Outlet /> */}
+      {/* <Footer /> */}
     </div>
   );
-}
+};
+//create browser  router mei array jaata hai , children saare outlet mei aate hai 
+// const AppRouter = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Body />
+//       },
+//       {
+//         path: "/about",
+//         element: <About />
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />
+//       },
+//       {
+//         path: "/restaurant/:id",
+//         element: <RestaurantMenu />
+//       },
+//     ],
+//   },
+// ]);
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<RouterProvider router={AppRouter} />); // render RouterProvider and use router as props and pass value appRouter
+
 
 export default App;
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,21 @@
+import { FaHeart } from 'react-icons/fa';
+import './Header.css'
+
 const Footer = () => {
-    return (
-      <div className="footer">
-        Created By
-        <i className="fa-solid fa-heart"></i>
-        {/* <a href="https://www.linkedin.com/in/chetannada/" target="_blank"> */}
-          <a>
-        shubham 
-        </a>
-        <i className="fa-solid fa-copyright"></i>2023
-        <strong>
-          Food<span>Fire</span>
-        </strong>
-      </div>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <div className="footer">
+      Created By <i className="FaHeart"><FaHeart /></i>
+      {/* <i className="fa-solid fa-heart"></i> */}
+      {/* <a href="https://www.linkedin.com/in/chetannada/" target="_blank"> */}
+      <h4>
+        Shubham
+      </h4>
+      <i className="fa-solid fa-copyright"></i>2023
+      <strong>
+        Food<span>Fire</span>
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
