f46f9770db6cf76dd94356e69c3ad39d2d5320b1
Shubham <shubhammehta2003@gmail.com>
2023-06-10 22:54:29 +0530

    updated error fixing and added rest menu card

---
+++ src/Components/RestaurantMenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RestaurantMenu = () => {
+  return (
+    <div>
+        <h1>
+
+    RestaurantMenu
+        </h1>
+    </div>
+  )
+}
+
+export default RestaurantMenu
