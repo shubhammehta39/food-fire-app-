00e71daaaa8433bb4227252aa708a2d701516c18
Shubham <shubhammehta2003@gmail.com>
2023-07-25 23:55:14 +0530

    update

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,19 @@ export const IMG_CDN_URL =
 export const swiggy_api_URL =
 "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 export const shimmer_card_unit = 8;
+
+
+//kmkmkmkm
+
+export const swiggy_menu_api_URL =
+"https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+
+// shimmer card unit
+// export const shimmer_card_unit = 8;
+
+// shimmer Menu card unit
+export const shimmer_menu_card_unit = 4;
+
+// menu items api card type key
+export const MENU_ITEM_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
+export const RESTAURANT_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.Restaurant";
